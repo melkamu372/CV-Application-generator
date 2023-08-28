e813837d6465729acb97a136fdbbbae0a387f95e
melkamu372 <melkamu372@gmail.com>
2023-08-28 16:04:23 +0300

    add greeting function

---
+++ src/Greetings.jsx
@@ -0,0 +1,4 @@
+ function Greetings(){
+    return <h1> Hello world i am melkamu software engineer</h1>
+}
+export default Greetings;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './Greetings.jsx'
+import Greetings from './Greetings.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Greetings />
   </React.StrictMode>,
 )
