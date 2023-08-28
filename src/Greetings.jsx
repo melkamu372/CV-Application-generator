2ffbeb3105161ad4382e8f02154b46b9931b24ac
melkamu372 <melkamu372@gmail.com>
2023-08-28 16:47:41 +0300

    update the greetings file

--- src/Greetings.jsx
+++ src/Greetings.jsx
@@ -1,4 +1,17 @@
  function Greetings(){
-    return <h1> Hello world i am melkamu software engineer</h1>
+    return(
+        <>
+        <h1>Test title</h1>
+        <ol className="test-list">
+          <li>List item 1</li>
+          <li>List item 2</li>
+          <li>List item 3</li>
+        </ol>
+        <svg>
+        <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
+     </svg>
+        <form><input type="text"/></form>
+        </>
+    )
 }
 export default Greetings;
