9ffba929854e3fbbdb0ea732c073af95a35ae74d
melkamu372 <melkamu372@gmail.com>
2023-09-01 15:49:50 +0300

    change the  header part

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,33 +1,51 @@
 import { useState } from 'react'
 import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
 import '../styles/app.css'
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
+    <div className='container'>
+     <div className='row'>
+      <div className='col-md-6'>
+      <input
+  className="form-control"
+  type="text"
+  defaultValue="Readonly input here..."
+  aria-label="readonly input example"
+  readOnly=""
+/>
+      <div className="mb-3">
+    <label htmlFor="exampleFormControlInput1" className="form-label">
+      Email address
+    </label>
+    <input
+      type="email"
+      className="form-control"
+      id="exampleFormControlInput1"
+      placeholder="name@example.com"
+    />
+  </div>
+  <div className="mb-3">
+    <label htmlFor="exampleFormControlTextarea1" className="form-label">
+      Example textarea
+    </label>
+    <textarea
+      className="form-control"
+      id="exampleFormControlTextarea1"
+      rows={3}
+      defaultValue={""}
+    />
+  </div>
+      </div>     {/* end of column  */}
+      <div className='col-md-6'>
+      <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </div>
     </>
   )
 }
