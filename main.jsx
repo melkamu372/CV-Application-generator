3ac89e76d973c0cffa15813bf9d26b2f20f51971
melkamu372 <melkamu372@gmail.com>
2023-09-05 16:58:13 +0300

    change main page

--- main.jsx
+++ main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import "bootstrap/dist/css/bootstrap.css"
 import App from './src/component/App.jsx'
-import './src/styles/index.css'
-import "bootstrap/dist/css/bootstrap.css";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+
 import reactLogo from "../assets/react.svg";
 import "../styles/app.css";
 import  PeronalDetails from '../component/PersonalDetails'
 import  Education from '../component/Education'
 import  Experience from '../component/Experience'
+
 
 function App() {
   return (
