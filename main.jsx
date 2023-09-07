be16a1e8678f477746359e15d2ab109253eb474c
melkamu372 <melkamu372@gmail.com>
2023-09-07 17:00:21 +0300

    update main page

--- main.jsx
+++ main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/js/bootstrap.bundle.js"
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import App from './src/component/App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
