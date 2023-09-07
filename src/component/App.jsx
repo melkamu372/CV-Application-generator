aaafa495804ce3695ca9a117d8e95405c770276e
melkamu372 <melkamu372@gmail.com>
2023-09-07 17:00:39 +0300

    update application page

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,17 +1,25 @@
-
-import reactLogo from "../assets/react.svg";
+import { useState } from "react";
 import "../styles/app.css";
 import  PeronalDetails from '../component/PersonalDetails'
 import  Education from '../component/Education'
 import  Experience from '../component/Experience'
+import DisplayPersonalDetail from '../component/DisplayPeronalDetail'
 
-
-function App() {
+function App() { 
   return (
     <>
-    <PeronalDetails/> 
-    <Education/>
-    <Experience/>
+    <div className="container"> 
+    <div className="row">
+      <div className="col-md-6">
+     <PeronalDetails person={person} setPerson={setPersonalInfo} />
+      <Education/>
+      <Experience/>
+      </div>
+      <div className="col-md-6">
+       <DisplayPersonalDetail/>
+      </div>
+    </div>
+    </div>
 </>
   
   );
